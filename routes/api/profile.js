8c44e95eb58f9ebd2031b4e789c2c78c8a95f8c6
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-07-23 12:25:26 -0700

    add profile fields to profile,js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator/check')
 
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -21,5 +22,47 @@ try{
     res.status(500).send('Server Error');
 }
 });
+
+// POST route api/profile
+// POST create or update a user profile
+// Private Acess needs auth
+
+router.post('/',[ auth,[
+    check('status', 'Status is required').not().isEmpty(), check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()})
+    }
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    // Build Profile Object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+    if(skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    console.log(profileFields.skills);
+    res.send('Hello')
+ })
 
 module.exports = router;
