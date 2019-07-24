e194b15acc859a1207a428a132d388ed98928c32
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-07-24 13:47:20 -0700

    add unlike post route in posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -114,4 +114,29 @@ router.put('/like/:id', auth, async(req, res) => {
         res.status(500).send('Server Error')
     }
 })
+
+// PUT route api/posts/unlike/:id
+// UnLike a post
+// Private
+router.put('/unlike/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check if the post has already been liked by the user
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({ msg: 'Post has not been liked'})
+        }
+        
+        // get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1)
+
+        await post.save();
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
