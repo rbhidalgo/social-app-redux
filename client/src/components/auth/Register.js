38aecd6753f02ac775201a3f847a01b73151eb15
Rogelio Hidalgo <rhidalgo987@gmail.com>
2019-08-07 12:34:22 -0700

    add alerts to register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,12 @@ import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
+
 
 // import axios from 'axios';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -21,7 +23,7 @@ const Register = (props) => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
-            props.setAlert('Passwords do not match', 'danger')
+            setAlert('Passwords do not match', 'danger')
         } else {
             console.log('Sucess!!!')
             // const newUser = {
@@ -88,4 +90,7 @@ const Register = (props) => {
     );
 };
 
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+}
 export default connect(null, { setAlert })(Register);
